58eae4815623ebc4b8b8d9e8e0de3fc7e6ef05ea
poan <pibicha@163.com>
2017-09-19 21:47:08 +0800

    使用flask-login实现登陆、记住我功能

--- app/models.py
+++ app/models.py
@@ -21,8 +21,10 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    email = db.Column(db.String(64),unique=True,index=True)
+
     # 密码相关===》》
-    password_hash = db.Column(db.String)
+    password_hash = db.Column(db.String(128))
 
     @property
     def password(self):
@@ -30,7 +32,7 @@ class User(UserMixin, db.Model):
 
     @password.setter
     def password(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
