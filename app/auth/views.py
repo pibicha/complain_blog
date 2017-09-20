40175c0394cde1830fed8968ba2c42c0da8fab74
poan <panbenxing@feellike21.com>
2017-09-20 14:50:04 +0800

    使用请求钩子限制未注册或注册未激活的用户访问权限

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,23 +1,24 @@
-from flask import render_template,redirect,request,url_for,flash
-from flask.ext.login import login_user,login_required,logout_user,current_user
+from flask import render_template, redirect, request, url_for, flash
+from flask.ext.login import login_user, login_required, logout_user, current_user
 from . import auth
-from .forms import LoginForm,RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
 from .. import db
 from ..email import send_email
 
-@auth.route("/login",methods=['GET','POST'])
-def login():
 
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember_me.data)
+            login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('无效的用户名或密码')
 
-    return render_template('auth/login.html',form=form)
+    return render_template('auth/login.html', form=form)
+
 
 @auth.route("/logout")
 @login_required
@@ -26,7 +27,8 @@ def logout():
     flash("你已经退出登陆")
     return redirect(url_for("main.index"))
 
-@auth.route('/register',methods=['GET','POST'])
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -36,13 +38,14 @@ def register():
         db.session.add(user)
         db.session.commit()
         token = user.generate_confirmation_token()
-        send_email(user.email,'请确认你的账户','auth/email/confirm',user=user,
+        send_email(user.email, '请确认你的账户', 'auth/email/confirm', user=user,
                    token=token)
         flash("请确认你的邮箱")
         return redirect(url_for('main.index'))
-    return render_template('auth/register.html',form=form)
+    return render_template('auth/register.html', form=form)
 
-#确认注册
+
+# 确认注册
 @auth.route("/confirm/<token>")
 @login_required
 def confirm(token):
@@ -53,3 +56,20 @@ def confirm(token):
     else:
         flash("无效的注册信息")
     return redirect(url_for('main.index'))
+
+
+# 每次请求前检查登录状态和是否已经确认注册邮件
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint() != 'static':
+        return redirect(url_for("auth.uncofirmed"))
+
+
+@auth.route("/unconfirmed")
+def uncofirmed():
+    if current_user.is_anonymous() or current_user.confirmed:
+        return redirect(url_for("main.index"))
+    return render_template("auth/uncofirmed.html")
