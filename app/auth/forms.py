84e4977e5ed984069dc5508b56e7bad735c31145
poan <panbenxing@feellike21.com>
2017-09-20 11:52:35 +0800

    注册用户

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,33 @@
 from flask.ext.wtf import Form
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import Required,Length,Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length, Email, Regexp,EqualTo
+from wtforms import ValidationError
+from ..models import User
+
 
 class LoginForm(Form):
-    email = StringField('邮箱',validators=[Required(),Length(1,64),
-                                         Email()])
-    password = PasswordField('密码',validators=[Required()])
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+                                          Email()])
+    password = PasswordField('密码', validators=[Required()])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登陆')
+
+
+class RegistrationForm(Form):
+    email = StringField("邮箱", validators=[Required(), Length(1, 64),
+                                          Email()])
+    username = StringField("用户名", validators=[Required(), Length(1, 64),
+                                              Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                     '用户名必须是字母、数字、点和下划线组成')])
+    password = PasswordField('密码',validators=[Required(),EqualTo('password2',
+                                                                 message='两次输入的密码不相符！')])
+    password2 = PasswordField('确认密码',validators=[Required()])
+
+    submit = SubmitField('注册')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经被注册.')
+    def validate_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经被使用.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template,redirect,request,url_for,flash
 from flask.ext.login import login_user,login_required,logout_user
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm
 from ..models import User
+from .. import db
 
 @auth.route("/login",methods=['GET','POST'])
 def login():
@@ -23,3 +24,15 @@ def logout():
     logout_user()
     flash("你已经退出登陆")
     return redirect(url_for("main.index"))
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash("你现在可以登录了")
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form=form)
