cf1cb6dda9cdbac8d3d1001b5ccf257a098bc311
poan <panbenxing@feellike21.com>
2017-09-21 11:52:12 +0800

    使用flask-principal实现权限管理

--- app/__init__.py
+++ app/__init__.py
@@ -6,17 +6,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
 
+from flask_principal import Principal, Permission, RoleNeed
+
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 login_manager = LoginManager()
+# 使用principal实现权限管理
+principals = Principal()
+
+# Create a permission with a single Need, in this case a RoleNeed.
+user_permission = Permission(RoleNeed('User'))
+moderator_permission = Permission(RoleNeed('Moderator'))
+amdin_permission = Permission(RoleNeed('Admin'))
 
 # 自定义访问未授权资源时的闪现消息
 login_manager.login_message = "请先登录！"
 
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -28,6 +38,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    principals.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -36,4 +47,3 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint)
 
     return app
-
