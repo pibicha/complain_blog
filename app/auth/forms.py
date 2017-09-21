a6be170b48f01492b985843bd348b5e775aca744
poan <panbenxing@feellike21.com>
2017-09-21 18:07:56 +0800

    表格项缺失提示语——update

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,22 +6,22 @@ from ..models import User
 
 
 class LoginForm(Form):
-    email = StringField('邮箱', validators=[Required(), Length(1, 64),
+    email = StringField('邮箱', validators=[Required("必填项"), Length(1, 64),
                                           Email()])
-    password = PasswordField('密码', validators=[Required()])
+    password = PasswordField('密码', validators=[Required("必填项")])
     remember_me = BooleanField('记住我')
     submit = SubmitField('登陆')
 
 
 class RegistrationForm(Form):
-    email = StringField("邮箱", validators=[Required(), Length(1, 64),
+    email = StringField("邮箱", validators=[Required("必填项"), Length(1, 64),
                                           Email()])
-    username = StringField("用户名", validators=[Required(), Length(1, 64),
+    username = StringField("用户名", validators=[Required("必填项"), Length(1, 64),
                                               Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                                      '用户名必须是字母、数字、点和下划线组成')])
-    password = PasswordField('密码',validators=[Required(),EqualTo('password2',
+    password = PasswordField('密码',validators=[Required("必填项"),EqualTo('password2',
                                                                  message='两次输入的密码不相符！')])
-    password2 = PasswordField('确认密码',validators=[Required()])
+    password2 = PasswordField('确认密码',validators=[Required("必填项")])
 
     submit = SubmitField('注册')
 
@@ -34,17 +34,17 @@ class RegistrationForm(Form):
 
 # 修改密码
 class ChangePasswordForm(Form):
-    old_password = PasswordField('原密码', validators=[Required()])
+    old_password = PasswordField('原密码', validators=[Required("必填项")])
     password = PasswordField('新密码', validators=[
-        Required(), EqualTo('password2', message='两次输入的密码不匹配')])
-    password2 = PasswordField('确认新密码', validators=[Required()])
+        Required("必填项"), EqualTo('password2', message='两次输入的密码不匹配')])
+    password2 = PasswordField('确认新密码', validators=[Required("必填项")])
     submit = SubmitField('更新密码')
 
 # 更新邮箱
 class ChangeEmailForm(Form):
-    email = StringField('新的邮箱地址', validators=[Required(), Length(1, 64),
+    email = StringField('新的邮箱地址', validators=[Required("必填项"), Length(1, 64),
                                                  Email()])
-    password = PasswordField('密码', validators=[Required()])
+    password = PasswordField('密码', validators=[Required("必填项")])
     submit = SubmitField('更新邮箱地址')
 
     def validate_email(self, field):
--- app/main/forms.py
+++ app/main/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import Required
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[Required("必填项")])
     submit = SubmitField('Submit')
