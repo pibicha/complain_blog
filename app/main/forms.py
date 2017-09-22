b80f1f164cfa35a182fea419b8f5c09481aaad59
poan <panbenxing@feellike21.com>
2017-09-22 12:03:39 +0800

    用户资料页——编辑

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import Required, Length, Email, Regexp, ValidationError,DataRequired
+from ..models import User, Role
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required("必填项")])
     submit = SubmitField('Submit')
+
+
+# 普通用户的编辑资料
+class EditProfileForm(FlaskForm):
+    name = StringField('真实姓名', validators=[Length(0, 64)])
+    location = StringField('家庭住址', validators=[Length(0, 64)])
+    about_me = TextAreaField('个性签名')
+    submit = SubmitField('提交')
+
+
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, session, redirect, url_for, current_app, abort
+from flask import render_template, session, redirect, url_for, current_app, abort, flash
 from .. import db
 from ..models import User
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
+
+from flask_login import login_required, current_user
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -16,3 +18,23 @@ def user(username):
     if user is None:
         abort(404)
     return render_template("user.html", user=user)
+
+
+# 普通用户的编辑资料
+@main.route("/edit-profile", methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash("更新成功！")
+        return redirect(url_for(".user",username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me = current_user.about_me
+    return render_template("edit_profile.html",form=form)
+
+#　管理员级别的编辑资料
