ddb0f7dc263c2f2e592e7a4f91799a476befcf23
poan <panbenxing@feellike21.com>
2017-09-27 12:02:38 +0800

    在heroku server上创建的目录、文件跟web app根本不在一个环境里。所以，db migration三条命令(init/migrate/uprade)必须在Bash里一并运行完，不能一条条运行

--- manage.py
+++ manage.py
@@ -22,7 +22,10 @@ manager.add_command('db', MigrateCommand)
 def deploy():
     from flask_migrate import upgrade
     from app.models import User
-    # 把数据库迁移到最新修订版本
+    # 把数据库迁移到最新修订版本 ,
+    # 在heroku server上创建的目录、文件跟web app根本不在一个环境里。所以，db migration三条命令(init/migrate/uprade)必须在Bash里一并运行完，不能一条条运行
+    db.init()
+    migrate()
     upgrade()
     # 让所有用户都关注此用户
     User.add_self_follows()
